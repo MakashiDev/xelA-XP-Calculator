17721e760fd060baad1e00bbf038f0be3efda5fc
Makashi <hg52254@gmail.com>
2021-12-03 14:05:24 -0700

    Added localstorage

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // index.html script
+document.getElementsByClassName("submit")[0].onclick = function() {
+    localStorage.setItem("xp", document.getElementById("current-xp").value);
+    localStorage.setItem("date", document.getElementById("time-join").value);
+    localStorage.setItem("level", document.getElementById("level").value);
+};
